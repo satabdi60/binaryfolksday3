4251d497110ce6ed3362d1056a6c580705686943
Satabai Roy <52540676+satabdi60@users.noreply.github.com>
2020-07-28 22:08:10 +0530

    Create StringReplace.php

---
+++ StringReplace.php
@@ -0,0 +1,11 @@
+<?php 
+  
+// Input string 
+$subjectVal = "It was nice meeting you. May you shine bright."; 
+  
+// using str_replace() function 
+$resStr = str_replace('you', 'him', $subjectVal); 
+  
+print_r($resStr); 
+  
+?> 
